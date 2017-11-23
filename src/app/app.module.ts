c54212fa1d52bb0d16d75cdd959a5eecfa362e1b
Ronaldo <ronaldosf.sf@gmail.com>
2017-11-23 12:42:59 -0300

    Erros de compilacao consertados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-import {ButtonModule} from 'primeng/primeng';
-import {CalendarModule} from 'primeng/primeng';
+import { RouterModule, Routes } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {TieredMenuModule, MenuItem} from 'primeng/primeng';
-import {PanelMenuModule, MenuItem} from 'primeng/primeng';
-import {BreadcrumbModule, MenuItem} from 'primeng/primeng';
 
+import {ButtonModule} from 'primeng/primeng';
+import {CalendarModule} from 'primeng/primeng';
+import {MenuItem} from 'primeng/primeng';
+import {TieredMenuModule} from 'primeng/primeng';
+import {PanelMenuModule} from 'primeng/primeng';
+import {BreadcrumbModule} from 'primeng/primeng';
+
+import { AppComponent } from './app.component';
+import { UsuarioModule } from './usuario/usuario.module';
+
+const routes: Routes = [
+  { path: 'home', component: AppComponent },  
+];
 
 @NgModule({
   declarations: [
@@ -21,7 +28,8 @@ import {BreadcrumbModule, MenuItem} from 'primeng/primeng';
   imports: [
     BrowserModule, BrowserAnimationsModule, 
     ButtonModule, CalendarModule, 
-    MatSidenavModule, MatIconModule, MatToolbarModule, TieredMenuModule, PanelMenuModule, BreadcrumbModule
+    MatSidenavModule, MatIconModule, MatToolbarModule, TieredMenuModule, PanelMenuModule, BreadcrumbModule,
+    UsuarioModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsuarioRoutingModule } from './usuario-routing.module';
+import { ListUserComponent } from './list.usuario.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'users', component: ListUserComponent },
+];
 
 @NgModule({
   imports: [
     CommonModule,
-    UsuarioRoutingModule
+    RouterModule.forChild(routes)
   ],
-  declarations: [],
-  bootstrap: [AppComponent]
+  declarations: [ListUserComponent],
+  bootstrap: [ListUserComponent]
 })
 export class UsuarioModule { }
